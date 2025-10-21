a55b561c147c9b6d92b97281f1c3e055f842fb56
martwijnman <153086626+martwijnman@users.noreply.github.com>
2025-10-21 13:39:01 +0200

    Models
    I'd made the models inclusive the id's.

---
+++ BarrocIntens/Models/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrocIntens.Models
+{
+    internal class Dashboard
+    {
+        public int Id { get; set; }
+    }
+}
