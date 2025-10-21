e9064ad8a578f6872af641a492d85271f8a28ba5
martwijnman <153086626+martwijnman@users.noreply.github.com>
2025-10-21 14:07:32 +0200

    Create AppDbContext.cs
    I've made the AppDbContext what is the one what made the migrations in the database. Also I made the lists what need to be require into the project. But I also made a comment to let the next teamate know that he have to make the models.

---
+++ BarrocIntens/Models/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrocIntens.Models
+{
+    internal class AppDbContext : DbContext
+    {
+        // These are the lists of the database
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Dashboard> Dashboards { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Malfunction> Malfunctions { get; set; }
+        public DbSet<Planning> Plannings { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // building the database
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=;database=csd_BarrocIntens",
+                ServerVersion.Parse("8.0.30")
+            );
+        }
+        // models are required here
+
+    }
+}
