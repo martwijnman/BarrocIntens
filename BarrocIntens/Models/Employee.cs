2dbd3b9bf6d42457acebd1185cece13d6e30d5ec
martwijnman <153086626+martwijnman@users.noreply.github.com>
2025-10-21 14:08:06 +0200

    Update Employee.cs
    I made columns of the database of Employee

--- BarrocIntens/Models/Employee.cs
+++ BarrocIntens/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,34 @@ namespace BarrocIntens.Models
 {
     internal class Employee
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(280)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(280)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        //[Required]
+        // add role
+
+        [Required]
+        public int AppointmentId { get; set; }
+
+        [Required]
+        public Appointment Appointment { get; set; }
+
+        //[Required]
+        // add tasks
+
+
+        public List<Appointment> Appointments { get; set; }
     }
 }
