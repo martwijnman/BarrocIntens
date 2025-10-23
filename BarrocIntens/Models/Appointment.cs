5cbed2b57f1f213ac54da31c1f9d69edd140cdd0
justinvanee123 <justinvnee123@gmail.com>
2025-10-23 17:54:07 +0200

    Models
    Models Appointments en Malfunctions gemaakt

--- BarrocIntens/Models/Appointment.cs
+++ BarrocIntens/Models/Appointment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,15 @@ namespace BarrocIntens.Models
     internal class Appointment
     {
         public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee employee { get; set; }
+        public DateTime Date { get; set; }
+        public string location { get; set; }
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+        public string Type { get; set; }
+        public int Time { get; set; }
     }
 }
--- BarrocIntens/Models/Malfunction.cs
+++ BarrocIntens/Models/Malfunction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,15 @@ namespace BarrocIntens.Models
     internal class Malfunction
     {
         public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public bool status { get; set; }
+        public string Action { get; set; }
+        public string Urgency { get; set; }
+
     }
 }
