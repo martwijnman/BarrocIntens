3d6de4ad0e59d1c39d611277504121d2a0702f89
martwijnman <153086626+martwijnman@users.noreply.github.com>
2025-11-06 10:42:10 +0100

    Create EmployeeValidator.cs

---
+++ BarrocIntens/Data/Validation/EmployeeValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using FluentValidation;
+
+namespace BarrocIntens.Data.Validation
+{
+    internal class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .WithMessage("Name is required.");
+
+            RuleFor(e => e.Password)
+                .NotEmpty()
+                .When(e => !string.IsNullOrEmpty(e.Name))
+                .WithMessage("Password is required.");
+        }
+    }
+}
